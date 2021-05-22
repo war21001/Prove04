4d228b97b47fbae116f6581d07d0cb126d89ff62
knight-captain <44849526+knight-captain@users.noreply.github.com>
2021-05-22 13:08:32 -0400

    responds to prove04

--- src/ward/Main.java
+++ src/ward/Main.java
@@ -3,6 +3,8 @@ package ward;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello, my name is Lisa.");
+
+        System.out.println("Hello, my name is Lisa.");
+        System.out.format("Hello %s, my name is %s.", "Lisa", "Captain");
     }
 }
